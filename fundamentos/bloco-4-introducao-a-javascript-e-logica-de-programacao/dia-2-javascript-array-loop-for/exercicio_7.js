4b14aa913229faa29dd0e17c73a9997bcc0091b8
Robson Narcizo <rob.narcizo.27@gmail.com>
2022-06-22 18:05:21 -0300

    Exercicio 7 - Finalizado

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-loop-for/exercicio_7.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let smaller = numbers[0];
+
+for(number of numbers){
+  if(number < smaller){
+    smaller = number
+  }
+}
+
+console.log('O menor numero contido no array é o ' + smaller);
