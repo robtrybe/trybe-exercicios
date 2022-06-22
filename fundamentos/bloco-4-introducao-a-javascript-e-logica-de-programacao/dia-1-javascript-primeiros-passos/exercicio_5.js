71396475dcd46e4c99aef0c8a8a743a7780b5a54
Robson Narcizo <rob.narcizo.27@gmail.com>
2022-06-22 11:45:52 -0300

    Exercicio 5 - Correção

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio_5.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio_5.js
@@ -2,7 +2,7 @@ const angulo1 = 45;
 const angulo2 = 45;
 const angulo3 = 90;
 
-if(angulo1 < 0 || angulo2 < 0 || angulo3 < 0 ){
+if(angulo1 <= 0 || angulo2 <= 0 || angulo3 <= 0 ){
   console.error('Um dos angulos informados é inválido!');
 }else if(angulo1 + angulo2 + angulo3 === 180){
   console.log(true);
