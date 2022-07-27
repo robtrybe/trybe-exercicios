34561fe45ab3cd90f5c8d8bd1554495572baa11f
Robson Narcizo <rob.narcizo.27@gmail.com>
2022-07-27 19:28:06 -0300

    Exercicio 7 - Finalizado

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/exercicio-6.js
@@ -0,0 +1,6 @@
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+const toObject = ([model , mark , year ]) => ({ model , mark , year}); 
+console.log(toObject(palio));
